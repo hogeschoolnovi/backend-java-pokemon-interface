756fc03a4a73737b5e199b840f7581ef079e8a7a
MRensen <markrensen@hotmail.com>
2022-07-14 21:00:16 +0200

    version 2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+
 
     public static void main(String[] args) {
         Scanner speler_A = new Scanner(System.in);
@@ -19,7 +24,7 @@ public class Main {
         names.add(player1.getVenusaur());
         player1.setPokemons(names);
         System.out.println("To start your game, we have given you 6 Pokemons use");
-        System.out.println("this are the Pokemons you get:");
+        System.out.println("these are the Pokemons you get:");
         pokemonGym.printPokemon(names);
         System.out.println("Would you like to enter a gym looking for a fight?");
         System.out.println("Please enter yes or no");
