3b04c093313a75654b97ccdec57f1232cfd44298
MRensen <markrensen@hotmail.com>
2022-12-14 13:11:46 +0100

    changed structure

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class Main {
+    // Deze 4 constanten worden gebruikt om tekst met kleur te printen in de console.
+    // Hier hoef je niks mee te doen
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
 
 
+    // Los in de main methode alle foutmeldigen op door (abstracte) klassen met variabelen en methoden te maken (en soms een import).
+    // In de main methode en de Main klasse hoef je niks te veranderen.
     public static void main(String[] args) {
         Scanner speler_A = new Scanner(System.in);
         PokemonGymImpl pokemonGym = new PokemonGymImpl();
--- src/Variabelen.java
+++ src/Variabelen.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.List;
 
+// Deze klasse dient als naslagwerk en dient uiteindelijk verwijderd te worden voor je het huiswerk inlevert.
+// In deze klasse staan een aantal variabelen die in meerdere klassen voorkomen en een aantal variabelen die slechts één keer gebruikt kunnen worden.
 public class Variabelen {
     /*deze komen meerdere keren voor*/
     type; /*""*/
